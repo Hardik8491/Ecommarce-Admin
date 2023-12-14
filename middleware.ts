33fa24dee74c60be7fcff58bf7d8dcfa5b2d32f8
Hardik Bhammar <94770400+Hardik8491@users.noreply.github.com>
2023-12-14 21:11:40 +0530

    auth middle ware edied

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/(api|trpc)(.*)"],
+  ignoredRoutes: ["/(api|trpc)(.*)"],
 });
 
 export const config = {
-  matcher: [],
-  // "/((?!.*\\..*|_next).*)", "/"
+  matcher: ["/((?!.*\\..*|_next).*)", "/", ],
 };
