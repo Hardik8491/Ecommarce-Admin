595ff34e6950e251980c4f8b5ed58e016a030315
Hardik Bhammar <94770400+Hardik8491@users.noreply.github.com>
2023-12-14 13:41:47 +0530

    update midilewere.ts

--- middleware.ts
+++ middleware.ts
@@ -5,10 +5,10 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
   
-  publicRoutes: ["/api/:path*"],
+  publicRoutes: ["*"],
 
 });
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(trpc)(.*)"],
+  matcher: [],
 };
