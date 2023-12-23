447161027aebd1edb2662c66b664021ac1b6bf19
Hardik Bhammar <94770400+Hardik8491@users.noreply.github.com>
2023-12-23 21:08:21 +0530

    middlewere update

--- app/api/[storeId]/checkout/public/route.ts
+++ app/api/[storeId]/checkout/public/route.ts
@@ -1,26 +1,83 @@
+import Stripe from "stripe";
 import { NextResponse } from "next/server";
-import { auth } from "@clerk/nextjs";
 
+import { stripe } from "@/lib/stripe";
 import prismadb from "@/lib/prismadb";
 
-export async function GET(
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
+export async function OPTIONS() {
+  return NextResponse.json({}, { headers: corsHeaders });
+}
+
+export async function POST(
   req: Request,
   { params }: { params: { storeId: string } }
 ) {
-  try {
-    if (!params.storeId) {
-      return new NextResponse("Store id is required", { status: 400 });
-    }
+  const { productIds } = await req.json();
 
-    const categories = await prismadb.category.findMany({
-      where: {
-        storeId: params.storeId,
-      },
-    });
-
-    return NextResponse.json(categories);
-  } catch (error) {
-    console.log("[CATEGORIES_GET]", error);
-    return new NextResponse("Internal error", { status: 500 });
+  if (!productIds || productIds.length === 0) {
+    return new NextResponse("Product ids are required", { status: 400 });
   }
-}
+
+  const products = await prismadb.product.findMany({
+    where: {
+      id: {
+        in: productIds
+      }
+    }
+  });
+
+  const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
+
+  products.forEach((product) => {
+    line_items.push({
+      quantity: 1,
+      price_data: {
+        currency: 'USD',
+        product_data: {
+          name: product.name,
+        },
+        unit_amount: product.price.toNumber() * 100
+      }
+    });
+  });
+
+  const order = await prismadb.order.create({
+    data: {
+      storeId: params.storeId,
+      isPaid: false,
+      orderItems: {
+        create: productIds.map((productId: string) => ({
+          product: {
+            connect: {
+              id: productId
+            }
+          }
+        }))
+      }
+    }
+  });
+
+  const session = await stripe.checkout.sessions.create({
+    line_items,
+    mode: 'payment',
+    billing_address_collection: 'required',
+    phone_number_collection: {
+      enabled: true,
+    },
+    success_url: `${process.env.FRONTEND_STORE_URL}/cart?success=1`,
+    cancel_url: `${process.env.FRONTEND_STORE_URL}/cart?canceled=1`,
+    metadata: {
+      orderId: order.id
+    },
+  });
+
+  return NextResponse.json({ url: session.url }, {
+    headers: corsHeaders
+  });
+};
