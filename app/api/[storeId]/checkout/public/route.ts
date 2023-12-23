d975d0205ab775bc469f7960226af4d3fe22e75e
Hardik Bhammar <94770400+Hardik8491@users.noreply.github.com>
2023-12-23 21:09:00 +0530

    middlewere update

--- app/api/[storeId]/checkout/public/route.ts
+++ app/api/[storeId]/checkout/public/route.ts
@@ -42,7 +42,7 @@ export async function POST(
         product_data: {
           name: product.name,
         },
-        unit_amount: product.price.toNumber() * 100
+        unit_amount: parseFloat(product.price) * 100
       }
     });
   });
